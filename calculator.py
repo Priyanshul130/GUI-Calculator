92f992089ea2580e950217fbd3d0484378bf4bf6
Priyanshul130 <piyanshul1307@gmail.com>
2024-08-12 00:08:18 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,38 +4,34 @@
 from tkinter import *
 from tkinter.ttk import *
 
-#create the window
+
 window=Tk()
 window.geometry("500x500")
 window.title("calculater")
 
-#label text for title
-title=Label(window,text = "BASIC CALCULATER")
 
-#label 1
+title=Label(window,text = "BASIC CALCULATER")
 caption1=Label(window,text = "first value-")
 
-#label 2
+
 caption2=Label(window,text = "second value-")
 
-#CREATInG ENTRY BUTTON
 entry1=Entry(window)
 entry2=Entry(window)
 
-#CEATEING RESULT BUTTON
 res=Label(window,text= "result")
 result=Label(window)
 
-#place labels
+
 title.grid(row=0,column=1,columnspan=3,pady=50)
 caption1.grid(row=1,column=0,padx=25)
 caption2.grid(row=2,column=0)
 
-#ENTRY PLACE
+
 entry1.grid(row=1,column=2)
 entry2.grid(row=2,column=2)
 
-#RESULT BUTTON
+
 res.grid(row=3,column=1)
 result.grid(row=3,column=2)
 
@@ -59,17 +55,16 @@ def div():
     num2=float(entry2.get())
     result.configure(text=num1/num2)
 
-#button
+
 add=Button(window,text="add",command=add)
 sub=Button(window,text="sub",command=sub)
 mul=Button(window,text="multiply",command=mul)
 div=Button(window,text="divide",command=div)
 
-#place radiobutton and combobox
+
 add.grid(row=1,column=4,padx=30)
 sub.grid(row=2,column=4,padx=30)
 mul.grid(row=3,column=4,padx=30)
 div.grid(row=4,column=4,padx=30)
 
-#calling main loop
 window.mainloop()
